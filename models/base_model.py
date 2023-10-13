4f88beee300cde93296ddd8700d6b29ce97393d0
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-10-13 14:23:29 +0200

    current workings

--- models/base_model.py
+++ models/base_model.py
@@ -1,58 +1,4 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-import models
-from datetime import datetime
-import uuid
-
-
-class BaseModel:
-    def __init__(self, *args, **kwargs) -> None:
-        """The constructor"""
-
-        if kwargs is not None and len(kwargs) != 0:
-            for key, val in kwargs.items():
-                if key == 'id':
-                    self.id = val
-                elif key == 'created_at':
-                    self.created_at = datetime.fromisoformat(val)
-                elif key == 'updated_at':
-                    self.updated_at = datetime.fromisoformat(val)
-                elif key == '__class__':
-                    # __class__ should not be added as an attribute
-                    continue
-                else:
-                    setattr(self, key, val)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.utcnow()
-            self.updated_at = datetime.utcnow()
-            models.storage.new(self)
-
-    def __str__(self) -> str:
-        """
-        Returns a string representation of the class instance
-        """
-        return "[{}] ({}) {}".format(self.__class__.__name__,
-                                     self.id, self.__dict__)
-
-    def save(self) -> None:
-        """
-        updates the public instance attribute
-        updated_at with the current datetime
-        """
-        self.updated_at = datetime.utcnow()
-        models.storage.save()
-
-    def to_dict(self) -> dict:
-        """
-        Returns a dictionary containing all keys/values of
-        __dict__ of the instance
-        """
-        new_dict = {k: v.isoformat() if k == 'created_at' or k == 'updated_at'
-                    else v for (k, v) in self.__dict__.items()}
-        new_dict['__class__'] = self.__class__.__name__
-        return new_dict
-=======
 """the base module"""
 
 from uuid import uuid4 as id4
@@ -117,4 +63,3 @@ class BaseModel:
         dictClass['created_at'] = dt.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
         dictClass['updated_at'] = dt.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
         return dictClass
->>>>>>> dae40e51c191d141474748eb5dcd95cfb95a8ee8
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,34 +1,4 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-from models.base_model import BaseModel
-import json as js
-import os
-
-class FileStorage:
-    __file_path = "file.json" # string - path to the JSON file (ex: file.json)
-    __objects = {} # dictionary - empty but will store all objects by <class name>.id
-
-    def all(self) -> dict:
-        """ returns the dictionary __objects"""
-        return FileStorage.__objects
-    
-    def new(self, obj) -> None:
-        """sets in __objects the obj with key <obj class name>.id"""
-        id = obj.id
-        class_name = obj.__class__.__name__
-        key = class_name + '.' + id
-        FileStorage.__objects[key] = obj
-
-    def save(self) -> None:
-        """: serializes __objects to the JSON file (path: __file_path)"""
-        with open(file=FileStorage.__file_path, mode='w', encoding='utf-8') as js_file:
-            json_dict = {key: val.to_dict() for (key, val) in FileStorage.__objects.items()}
-            js.dump(json_dict, js_file)
-
-    def reload(self) -> None:
-        """
-        deserializes the JSON file to __objects (only if the JSON file (__file_path) exists ;
-=======
 """file storange json"""
 import json
 from models.base_model import BaseModel
@@ -67,24 +37,13 @@ class FileStorage:
     def reload(self):
         """ deserializes the JSON file to __objects
         (only if the JSON file (__file_path) exists ;
->>>>>>> dae40e51c191d141474748eb5dcd95cfb95a8ee8
         otherwise, do nothing. If the file doesn’t exist,
         no exception should be raised)
         """
         try:
-<<<<<<< HEAD
-            with open(file=FileStorage.__file_path, mode='r', encoding='utf-8') as js_file:
-                for key, val in js.load(js_file).items():
-                    instance = eval(val['__class__'])(**val)
-                    self.__objects[key] = instance
-        except FileNotFoundError:
-            pass
-
-=======
             with open(self.__file_path, 'r', encoding='utf-8') as file:
                 for key, value in json.load(file).items():
                     construtor = eval(value['__class__'])(**value)
                     self.__objects[key] = construtor
         except FileNotFoundError:
             pass
->>>>>>> dae40e51c191d141474748eb5dcd95cfb95a8ee8
