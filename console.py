d7bf436764f092b5fd100a88a64f755b83b78003
Kayr0r404 <123119832+Kayr0r404@users.noreply.github.com>
2023-10-14 14:25:44 +0200

    Update console.py

--- console.py
+++ console.py
@@ -35,6 +35,7 @@ class HBNBCommand(cmd.Cmd):
     def do_quit(self, line) -> None:
         """Quit command to exit the program
         """
+        print()
         exit(1)
 
     def do_EOF(self, line) -> bool:
