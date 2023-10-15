cc38341b8dabaaaf951597e77a5dec716cfd42ea
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-10-15 21:28:40 +0200

    testing

--- tests/test_console.py
+++ tests/test_console.py
@@ -10,7 +10,27 @@ import os
 
 class CreateTests(unittest.TestCase):
     '''Create Tests'''
-    pass
+    def test_no_input(self):
+        '''test when only create is parsed on the console'''
+        output = '** class name missing **'
+        with patch('builtins.print') as mock_print:
+            HBNBCommand().onecmd('create')
+            mock_print.assert_called_once_with(output)
+
+    def test_invalid_cmd(self):
+        '''parse invalid command'''
+        output = '** class doesn\'t exist **'
+        with patch('builtins.print') as mock_print:
+            HBNBCommand().onecmd('create user')
+            mock_print.assert_called_once_with(output)
+
+    def test_valid_input(self):
+        '''test when valid inputs are parsed to the console
+        we'll use type of output to test'''
+        with patch('builtins.print') as mock_print:
+            HBNBCommand().onecmd('create User')
+            output = mock_print.call_args[0][0]
+            self.assertIsInstance(output, str)
 
 
 class AllTests(unittest.TestCase):
@@ -26,6 +46,8 @@ class DestroyTests(unittest.TestCase):
 
 
 class UpdateTests(unittest.TestCase):
+    '''Update test cases'''
+    # def test_
     pass
 
 
@@ -34,6 +56,8 @@ class QuitTest(unittest.TestCase):
 
     def test_quit_exits(self):
         with patch("sys.stdout", new=StringIO()) as output:
+            print('sd')
+            print(HBNBCommand().onecmd("quit"))
             self.assertTrue(HBNBCommand().onecmd("quit"))
 
     def test_EOF_exits(self):
